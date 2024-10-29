4330dffc364440004e684865191b2007a6ea8afb
reka114141 <reka114141@gmail.com>
2024-10-30 05:53:04 +0700

    Delete folder-lagu-sunda directory

--- folder-lagu-sunda/script.js
+++
@@ -1,6 +0,0 @@
-function playSong(songPath) {
-    const audioPlayer = document.getElementById('audio-player');
-    audioPlayer.src = songPath;
-    audioPlayer.play();
-    audioPlayer.hidden = false;
-}
